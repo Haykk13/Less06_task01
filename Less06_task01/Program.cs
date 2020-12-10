e0b08d16e299b4181b9a70b95ddfba899e3b56ed
Hayk <hayk.hayr3@gmail.com>
2020-12-10 23:28:42 +0400

    Add project files.

---
+++ Less06_task01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Less06_task01
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			MyClass inst = new MyClass();
+			MyClass.x = 10;
+
+			Console.WriteLine(MyClass.x);
+
+			Console.ReadKey();
+		}
+	}
+
+	class MyClass
+	{
+		public static int x = 5;
+	}
+}
+
